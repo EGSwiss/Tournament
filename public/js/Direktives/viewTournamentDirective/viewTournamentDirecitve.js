f0ad747af76e4ff1ee5bc793ef0c90f9decc34be
Michael Haenzi <michael.haenzi@geoinfo.ch>
2016-02-19 21:51:17 +0100

    Some changes

--- public/js/Direktives/viewTournamentDirective/viewTournamentDirecitve.js
+++ public/js/Direktives/viewTournamentDirective/viewTournamentDirecitve.js
@@ -1,21 +1,21 @@
 var module = angular.module('directives');
 
-module.directive('tournamentField', function () {
+module.directive('viewTournament', function () {
     return {
         restrict: 'E',
-        templateUrl: './html/Directive/tournamentField.html',
+        templateUrl: './html/Directive/viewTournament.html',
         scope: {
-            tournament: '='
+            viewTournament: '='
         },
         controller: function ($scope) {
             $scope.allTeams = [];
             $scope.allParticipants = [];
-            if ($scope.tournament.tournamenttype === 'team') {
-                _.each($scope.tournament.participants, function (team) {
+            if ($scope.viewTournament.tournamenttype === 'team') {
+                _.each($scope.viewTournament.participants, function (team) {
                     $scope.allTeams.push(team);
                 });
             } else {
-                _.each($scope.tournament.participants, function (participant) {
+                _.each($scope.viewTournament.participants, function (participant) {
                     $scope.allParticipants.push(participant);
                 });
             }
